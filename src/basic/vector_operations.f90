a4487c26e1788eda951735214a15b660fa73f55e
cmagnus25 <magnus.carlsson@chalmers.se>
2024-12-01 20:49:21 +0100

    Added further vector operations.

--- src/basic/vector_operations.f90
+++ src/basic/vector_operations.f90
@@ -5,9 +5,115 @@ module vector_operations
  public :: elltwo_norm
  public :: rms
  public :: length
+ public :: vector_res
+ public :: vector_u
+ public :: vector2du
+ public :: vector_dt_term
 
  contains
 
+!********************************************************************************
+ function vector_res(nnodes,nq) result(vector)
+
+ use edu2d_constants   , only : p2
+ use edu2d_my_main_data, only : node
+
+ implicit none
+
+!Input
+ integer          , intent( in) :: nnodes, nq
+
+!Output
+ real(p2), dimension(nnodes*nq) :: vector
+
+!local variables
+ integer                        :: i, k
+
+  do i = 1, nnodes
+   do k = 1, nq
+    vector( nq*(i-1) + k ) = node(i)%res(k)
+   end do
+  end do  
+
+ end function vector_res
+!********************************************************************************
+
+!********************************************************************************
+!* This function returns a global vector of residual.
+!********************************************************************************
+ subroutine vector_u(vector)
+
+ use edu2d_constants   , only : p2
+ use edu2d_my_main_data, only : node, nnodes, nq
+
+ implicit none
+
+!Output
+ real(p2), dimension(nnodes*nq), intent(out) :: vector
+
+!local variables
+ integer                                     :: i, k
+
+  do i = 1, nnodes
+   do k = 1, nq
+    vector( nq*(i-1) + k ) = node(i)%u(k)
+   end do
+  end do  
+
+ end subroutine vector_u
+!********************************************************************************
+
+
+!********************************************************************************
+!* This function returns a global vector of residual.
+!********************************************************************************
+ subroutine vector2du(vector,nnodes,nq)
+
+ use edu2d_constants   , only : p2
+ use edu2d_my_main_data, only : node
+
+ implicit none
+
+!Input
+ integer                       , intent( in) :: nnodes, nq
+ real(p2), dimension(nnodes*nq), intent( in) :: vector
+
+!local variables
+ integer                                     :: i, k
+
+  do i = 1, nnodes
+   do k = 1, nq
+    node(i)%du(k) = vector( nq*(i-1) + k )
+   end do
+  end do  
+
+ end subroutine vector2du
+ !********************************************************************************
+ subroutine vector_dt_term(vector)
+
+ use edu2d_constants   , only : p2
+ use edu2d_my_main_data, only : node, nnodes, nq, CFL_frechet, CFL
+
+ implicit none
+
+!Output
+ real(p2), dimension(nnodes*nq), intent(out) :: vector
+
+!local variables
+ integer                                     :: i, k
+ real(p2) :: temp
+
+  CFL_frechet = CFL
+
+  do i = 1, nnodes
+   temp = node(i)%vol / ( CFL_frechet*node(i)%dt)
+
+   do k = 1, nq
+    vector( nq*(i-1) + k ) = temp
+   end do
+  end do  
+
+ end subroutine vector_dt_term
 !********************************************************************************
  function elltwo_norm(vec,n)
 
